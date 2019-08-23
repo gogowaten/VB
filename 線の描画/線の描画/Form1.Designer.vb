5745743bb1602815d0c453b49fa148e464c37286
waten <gogowaten@yahoo.co.jp>
2019-08-23 11:24:38 +0900

    201908231124

---
+++ 線の描画/線の描画/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(344, 227)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(387, 262)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ 線の描画/線の描画/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Private beginPoint As Point 'マウスドラッグ開始点記録用
+    Private myPen As New Pen(Brushes.Aqua, 3) '直線用のPen
+    Private myPoints As New List(Of Point) '頂点座標群記録用
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Call MyInitial()
+    End Sub
+
+    '初期化
+    Private Sub MyInitial()
+        '初期座標設定
+        myPoints = New List(Of Point)(New Point() {
+                                      New Point(0, 0),
+                                      New Point(150, 40),
+                                      New Point(80, 100),
+                                      New Point(100, 150)})
+        '頂点表示用のLabel作成
+        For i = 0 To myPoints.Count - 1
+            Call MakeLabel(myPoints(i), i)
+        Next
+
+        '直線を描画
+        Call MyDrawLines()
+
+    End Sub
+    '左クリック時の処理
+    Private Sub MyMouseDown(sender As Object, e As MouseEventArgs)
+        beginPoint = e.Location
+    End Sub
+
+    'マウスドラッグ中処理
+    Private Sub MyMouseMove(sender As Object, e As MouseEventArgs)
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Dim myLabel = DirectCast(sender, Label)
+            Dim newLocate As Point
+            newLocate = New Point(e.X - beginPoint.X + myLabel.Location.X,
+                                  e.Y - beginPoint.Y + myLabel.Location.Y)
+            myLabel.Location = newLocate
+            myPoints(myLabel.Tag) = newLocate
+            Call MyDrawLines()
+        End If
+    End Sub
+
+    '直線を描画
+    Private Sub MyDrawLines()
+        Dim canvas As New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+        Dim g As Graphics = Graphics.FromImage(canvas)
+        g.DrawLines(myPen, myPoints.ToArray)
+        g.Dispose()
+        Me.PictureBox1.Image = canvas
+
+    End Sub
+    ''' <summary>
+    ''' 頂点表示用のLabel作成
+    ''' </summary>
+    ''' <param name="locate">頂点座標</param>
+    ''' <param name="number">通し番号(何番目の頂点なのか識別用)</param>
+    ''' <returns></returns>
+    Private Function MakeLabel(locate As Point, number As Integer) As Point
+        Dim myLabel = New Label()
+        With myLabel
+            .Width = 10
+            .Height = 10
+            .BackColor = Color.Black
+            .Location = locate
+            .Tag = number 'Tagに通し番号を記録
+        End With
+        Me.PictureBox1.Controls.Add(myLabel)
+        'マウスイベント時の処理追加
+        AddHandler myLabel.MouseDown, AddressOf MyMouseDown
+        AddHandler myLabel.MouseMove, AddressOf MyMouseMove
+
+    End Function
+
+End Class
